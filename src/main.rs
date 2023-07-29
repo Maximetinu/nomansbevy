282154bd729a7fcd526300e85224df2a5f604711
Miguel Medina Ballesteros <maximetinu@gmail.com>
2023-07-29 23:14:53 +0200

    Setup physics example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 const TIME_STEP: f32 = 1.0 / 60.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((
+            DefaultPlugins,
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
+            RapierDebugRenderPlugin::default(),
+        ))
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
         .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
@@ -16,9 +21,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let player_handle: Handle<Image> = asset_server.load("sprites/player.png");
 
-    commands.spawn(SpriteBundle {
-        texture: player_handle.clone(),
-        transform: Transform::default(),
-        ..default()
-    });
+    // bouncing ball
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(SpriteBundle {
+            texture: player_handle.clone(),
+            transform: Transform::default(),
+            ..default()
+        });
+
+    // ground
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -200.0, 0.0)));
 }
