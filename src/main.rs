be63ae489d0f46f819f3dc1cc09a3841ed6c7482
Miguel Medina Ballesteros <maximetinu@gmail.com>
2023-07-29 22:47:03 +0200

    Fixed time step 60 FPS

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 
+const TIME_STEP: f32 = 1.0 / 60.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(FixedTime::new_from_secs(TIME_STEP))
         .run();
 }
