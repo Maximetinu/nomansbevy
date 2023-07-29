ea87551f5f390969afcca61aebbb98812a17db86
Miguel Medina Ballesteros <maximetinu@gmail.com>
2023-07-29 22:47:10 +0200

    Close on ESC

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
