11f06785526d2095362e842165f5dd39851b5527
Miguel Medina Ballesteros <maximetinu@gmail.com>
2023-07-29 22:58:10 +0200

    Add setup system, spawn player

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
+        .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let player_handle: Handle<Image> = asset_server.load("sprites/player.png");
+
+    commands.spawn((SpriteBundle {
+        texture: player_handle.clone(),
+        transform: Transform::default(),
+        ..default()
+    },));
 }
